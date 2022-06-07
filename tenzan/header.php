e3afa53583dcb7fa5a096cb1890480f568bc8be7
mahaupt <subcortexx@gmail.com>
2022-06-07 18:06:37 +0200

    Fix Menu Style

--- tenzan/header.php
+++ tenzan/header.php
@@ -16,7 +16,7 @@
   <?php get_template_part( 'templates/logo' ); ?>
 </header>
 <nav>
-<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'header-menu', 'container_class' => 'header-menu' ) ); ?>
 
 <div class="strike text-center">
    <span>all about Japanese culture</span>
